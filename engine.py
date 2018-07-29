1b1fae0391948b3d38e8176d5a7393a6de3cbffd
Christen Ward <christen@christenward.com>
2018-07-28 21:27:21 -0500

    initial commit

---
+++ engine.py
@@ -0,0 +1,404 @@
+import tcod.tcod as libtcod
+
+from components.equipment import Equipment
+from components.equippable import Equippable
+from components.level import Level
+from equipment_slots import EquipmentSlots
+from input_handlers import handle_keys, handle_mouse, handle_main_menu
+from entity import Entity, get_blocking_entities_at_location
+from menus import main_menu, message_box
+from render_functions import clear_all, render_all, RenderOrder
+from map_objects.game_map import GameMap
+from fov_functions import initialize_fov, recompute_fov
+from game_states import GameStates
+from components.fighter import Fighter
+from components.inventory import Inventory
+from death_functions import kill_monster, kill_player
+from game_messages import MessageLog, Message
+
+
+def main():
+    screen_width = 70
+    screen_height = 45
+    window_title = 'game'
+    panel_height = 10
+
+    libtcod.console_set_custom_font('assets/fonts/Taffer_20x20.png', libtcod.FONT_LAYOUT_ASCII_INROW)
+
+    libtcod.console_init_root(screen_width, screen_height, window_title, False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+    panel = libtcod.console_new(screen_width, panel_height)
+
+    player = None
+    entities = []
+    game_map = None
+    message_log = None
+    game_state = None
+
+    show_main_menu = True
+    show_load_error_message = False
+
+    main_menu_background_image = libtcod.image_load('assets/images/astro.png')
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
+
+        if show_main_menu:
+            main_menu(con, main_menu_background_image, screen_width,
+                      screen_height)
+
+            if show_load_error_message:
+                message_box(con, 'No save game to load', 50, screen_width, screen_height)
+
+            libtcod.console_flush()
+
+            action = handle_main_menu(key)
+
+            new_game = action.get('new_game')
+            load_saved_game = action.get('load_game')
+            exit_game = action.get('exit')
+
+            if show_load_error_message and (new_game or load_saved_game or exit_game):
+                show_load_error_message = False
+            elif new_game:
+                # player, entities, game_map, message_log, game_state = get_game_variables(constants)
+                game_state = GameStates.PLAYERS_TURN
+
+                show_main_menu = False
+            elif load_saved_game:
+                try:
+                    # player, entities, game_map, message_log, game_state = load_game()
+                    show_main_menu = False
+                except FileNotFoundError:
+                    show_load_error_message = True
+            elif exit_game:
+                break
+
+        else:
+            libtcod.console_clear(con)
+            play_game()
+
+            show_main_menu = True
+
+
+def play_game():
+    screen_width = 70
+    screen_height = 45
+
+    bar_width = 20
+
+    panel_horiz_height = 10
+    panel_horiz_y = screen_height - panel_horiz_height
+    panel_horiz = libtcod.console_new(screen_width, panel_horiz_height)
+
+    panel_vert_height = panel_horiz_y
+    panel_vert_y = 0
+    panel_vert = libtcod.console_new(48, panel_vert_height)
+
+    message_x = 1
+    message_width = screen_width - 1
+    message_height = panel_horiz_height - 2
+
+    map_width = 47
+    map_height = panel_horiz_y
+
+    room_max_size = 8
+    room_min_size = 3
+    max_rooms = 30
+
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
+    max_monsters_per_room = 3
+    max_items_per_room = 2
+
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
+    }
+
+    fighter_component = Fighter(hp=100, defense=1, power=2)
+    inventory_component = Inventory(26)
+    level_component = Level()
+    equipment_component = Equipment()
+
+    player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
+                    fighter=fighter_component, inventory=inventory_component, level=level_component,
+                    equipment=equipment_component)
+
+    entities = [player]
+
+    equippable_component = Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)
+    dagger = Entity(0, 0, '-', libtcod.sky, 'Dagger', equippable=equippable_component)
+    player.inventory.add_item(dagger)
+    player.equipment.toggle_equip(dagger)
+
+    libtcod.console_set_custom_font('assets/fonts/Taffer_20x20.png', libtcod.FONT_LAYOUT_ASCII_INROW)
+
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
+
+    con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities)
+
+    fov_recompute = True
+
+    fov_map = initialize_fov(game_map)
+
+    message_log = MessageLog(message_x, message_width, message_height)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    game_state = GameStates.PLAYERS_TURN
+    previous_game_state = game_state
+
+    targeting_item = None
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
+
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(con, panel_vert, panel_horiz, entities, player, game_map, fov_map, fov_recompute, message_log,
+                   screen_width, screen_height,
+                   bar_width, panel_horiz_height, panel_horiz_y, panel_vert_height, panel_vert_y, mouse, colors,
+                   game_state)
+
+        fov_recompute = False
+
+        libtcod.console_flush()
+
+        clear_all(con, entities)
+
+        action = handle_keys(key, game_state)
+        mouse_action = handle_mouse(mouse)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        pickup = action.get('pickup')
+        fullscreen = action.get('fullscreen')
+        show_inventory = action.get('show_inventory')
+        inventory_index = action.get('inventory_index')
+        drop_inventory = action.get('drop_inventory')
+        take_stairs = action.get('take_stairs')
+        level_up = action.get('level_up')
+        show_character_screen = action.get('show_character_screen')
+        wait = action.get('wait')
+
+        left_click = mouse_action.get('left_click')
+        right_click = mouse_action.get('right_click')
+
+        player_turn_results = []
+
+        if move and game_state == GameStates.PLAYERS_TURN:
+            dx, dy = move
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                if not game_map.is_blocked(destination_x, destination_y):
+                    target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+
+                    if target:
+                        attack_results = player.fighter.attack(target)
+                        player_turn_results.extend(attack_results)
+                    else:
+                        player.move(dx, dy)
+
+                        fov_recompute = True
+                    game_state = GameStates.ENEMY_TURN
+
+        elif wait:
+            message_log.add_message(Message('You wait for a moment.', libtcod.yellow))
+            game_state = GameStates.ENEMY_TURN
+
+        elif pickup and game_state == GameStates.PLAYERS_TURN:
+            for entity in entities:
+                if entity.item and entity.x == player.x and entity.y == player.y:
+                    pickup_results = player.inventory.add_item(entity)
+                    player_turn_results.extend(pickup_results)
+
+                    break
+            else:
+                message_log.add_message(Message('There is nothing here to pick up.', libtcod.yellow))
+
+        if show_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.SHOW_INVENTORY
+
+        if drop_inventory:
+            previous_game_state = game_state
+            game_state = GameStates.DROP_INVENTORY
+
+        if inventory_index is not None and previous_game_state != GameStates.PLAYER_DEAD and inventory_index < len(
+                player.inventory.items):
+            item = player.inventory.items[inventory_index]
+            if game_state == GameStates.SHOW_INVENTORY:
+                player_turn_results.extend(player.inventory.use(item, entities=entities, fov_map=fov_map))
+            elif game_state == GameStates.DROP_INVENTORY:
+                player_turn_results.extend(player.inventory.drop_item(item))
+
+        if take_stairs and game_state == GameStates.PLAYERS_TURN:
+            for entity in entities:
+                if entity.stairs and entity.x == player.x and entity.y == player.y:
+                    entities = game_map.next_floor(player, message_log, max_rooms, room_min_size, room_max_size,
+                                                   map_width, map_height)
+                    fov_map = initialize_fov(game_map)
+                    fov_recompute = True
+                    libtcod.console_clear(con)
+
+                    break
+            else:
+                message_log.add_message(Message('There are no stairs here.', libtcod.yellow))
+
+        if level_up:
+            if level_up == 'hp':
+                player.fighter.base_max_hp += 20
+                player.fighter.hp += 20
+            elif level_up == 'str':
+                player.fighter.base_power += 1
+            elif level_up == 'def':
+                player.fighter.base_defense += 1
+
+            game_state = previous_game_state
+
+        if show_character_screen:
+            previous_game_state = game_state
+            game_state = GameStates.CHARACTER_SCREEN
+
+        if game_state == GameStates.TARGETING:
+            if left_click:
+                target_x, target_y = left_click
+
+                item_use_results = player.inventory.use(targeting_item, entities=entities, fov_map=fov_map,
+                                                        target_x=target_x, target_y=target_y)
+                player_turn_results.extend(item_use_results)
+            elif right_click:
+                player_turn_results.append({'targeting_cancelled': True})
+
+        if exit:
+            if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY, GameStates.CHARACTER_SCREEN):
+                game_state = previous_game_state
+            elif game_state == GameStates.TARGETING:
+                player_turn_results.append({'targeting_cancelled': True})
+            else:
+                return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+        for player_turn_result in player_turn_results:
+            message = player_turn_result.get('message')
+            dead_entity = player_turn_result.get('dead')
+            item_added = player_turn_result.get('item_added')
+            item_consumed = player_turn_result.get('consumed')
+            item_dropped = player_turn_result.get('item_dropped')
+            targeting = player_turn_result.get('targeting')
+            targeting_cancelled = player_turn_result.get('targeting_cancelled')
+            xp = player_turn_result.get('xp')
+            equip = player_turn_result.get('equip')
+
+            if message:
+                message_log.add_message(message)
+
+            if dead_entity:
+                if dead_entity == player:
+                    message, game_state = kill_player(dead_entity)
+                else:
+                    message = kill_monster(dead_entity)
+
+                message_log.add_message(message)
+
+            if item_added:
+                entities.remove(item_added)
+
+                game_state = GameStates.ENEMY_TURN
+
+            if item_consumed:
+                game_state = GameStates.ENEMY_TURN
+
+            if targeting:
+                previous_game_state = GameStates.PLAYERS_TURN
+                game_state = GameStates.TARGETING
+
+                targeting_item = targeting
+
+                message_log.add_message(targeting_item.item.targeting_message)
+
+            if targeting_cancelled:
+                game_state = previous_game_state
+
+                message_log.add_message(Message('Targeting cancelled'))
+
+            if xp:
+                leveled_up = player.level.add_xp(xp)
+                message_log.add_message(Message('You gain {0} experience points.'.format(xp)))
+
+                if leveled_up:
+                    message_log.add_message(Message(
+                        'Your battle skills grow stronger! You reached level {0}'.format(
+                            player.level.current_level) + '!', libtcod.yellow))
+                    previous_game_state = game_state
+                    game_state = GameStates.LEVEL_UP
+
+            if item_dropped:
+                entities.append(item_dropped)
+
+                game_state = GameStates.ENEMY_TURN
+
+            if equip:
+                equip_results = player.equipment.toggle_equip(equip)
+
+                for equip_result in equip_results:
+                    equipped = equip_result.get('equipped')
+                    dequipped = equip_result.get('dequipped')
+
+                    if equipped:
+                        message_log.add_message(Message('You equipped the {0}'.format(equipped.name)))
+
+                    if dequipped:
+                        message_log.add_message(Message('You dequipped the {0}'.format(dequipped.name)))
+
+                game_state = GameStates.ENEMY_TURN
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity.ai:
+                    enemy_turn_results = entity.ai.take_turn(player, fov_map, game_map, entities)
+
+                    for enemy_turn_result in enemy_turn_results:
+                        message = enemy_turn_result.get('message')
+                        dead_entity = enemy_turn_result.get('dead')
+
+                        if message:
+                            message_log.add_message(message)
+
+                        if dead_entity:
+                            if dead_entity == player:
+                                message, game_state = kill_player(dead_entity)
+                            else:
+                                message = kill_monster(dead_entity)
+
+                            message_log.add_message(message)
+
+                            if game_state == GameStates.PLAYER_DEAD:
+                                break
+
+                    if game_state == GameStates.PLAYER_DEAD:
+                        break
+            else:
+                game_state = GameStates.PLAYERS_TURN
+
+
+if __name__ == '__main__':
+    main()
